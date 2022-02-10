35fe6a2aa96928028a0e87e13c2399badba1f1e2
Jose Cavani <Jose.Cavani@gmail.com>
2022-02-10 17:40:39 -0300

    fixed bug on line 42 cannot accept non cordnial positions

--- Tupiniquim/Program.cs
+++ Tupiniquim/Program.cs
@@ -40,6 +40,8 @@ namespace Tupiniquim
                     robo.X = int.Parse(posicao.Split(' ')[0]);
                     robo.Y = int.Parse(posicao.Split(' ')[1]);
                     robo.Orientacao = char.Parse(posicao.ToUpper().Split(' ')[2]);
+                    if (robo.Orientacao != 'l' && robo.Orientacao != 'O' && robo.Orientacao != 'N' && robo.Orientacao != 'S')
+                        throw new Exception();
                 }
                 catch (Exception)
                 {
